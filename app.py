a8280465e8eab7773664c2310bfa2f02aee55137
Anongnat R <anongnat.rat@ku.th>
2022-02-07 15:25:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+
+import json
 from random import randint
 from pythainlp import word_tokenize
 from flask import Flask, request, abort
-
+from argparse import ArgumentParser
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -14,11 +16,41 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-channel_secret = 'e03b6ab892f98d9e35000187a87987a6'
-channel_access_token = '7Qm9ky2ZN5SCTfoEip4I8mddUkmYubqC0tw3emoeATzHSn8njDqpRjfD5tFmOnZ7mdzI3w81eu3IIdyb96q+z5EsMFKdxCOfSJ4uVEkK7arXpbJkPItOE6HLqw+sDLttRvScCPWV485Y9fPkyd7ZOgdB04t89/1O/w1cDnyilFU='
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = '087de6c99fc50017802808a0a8c84e3e'
+channel_access_token = '1Epm1mCMcOi/TD5zF6vt7Ti1b0GK1gUQ4T8RaJdPRr/f97Xhz8j9KniHkF+5havAk73P8scdo0vmohex+RdpVvUD8VGpUlF2n2lyGwYphMKgh2dcP4C8KCqH95PhKWAHH3SF2ltPk4+uVElCwCUNJwdB04t89/1O/w1cDnyilFU='
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
-food_list = ['กระดูกอ่อนตุ๋นไชเท้ายาจีน',
+
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from linebot.exceptions import LineBotApiError
+
+@app.route('/webhook', methods=['POST'])
+def callback():
+    json_line = request.get_json(force=False,cache=False)
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    no_event = len(decoded['events'])
+    for i in range(no_event):
+        event = decoded['events'][i]
+        event_handle(event)
+    return '',200
+
+def event_handle(event):
+    print(event)
+    try:
+        userId = event['source']['userId']
+    except:
+        print('error cannot get userId')
+        return ''
+
+    try:
+        rtoken = event['replyToken']
+    except:
+        print('error cannot get rtoken')
+        return ''
+    food_list = ['กระดูกอ่อนตุ๋นไชเท้ายาจีน',
              'เกาเหลาลูกชิ้นหมู',
              'แกงจืดไข่น้ำ',
              'แกงจืดเต้าหู้ไข่สาหร่าย',
@@ -71,45 +103,18 @@ food_list = ['กระดูกอ่อนตุ๋นไชเท้ายา
              'ยำเห็ดหูหนูขาว',
              'ลาบหมู/ไก่/ปลาช่อน',
              'หมู/ไก่มะนาว']
-
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello Guys!"
-
-
-@app.route("/webhook", methods=['GET','POST'])
-def webhook():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
-
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    text = event.message.text
+    text = event['message']['text']
     proc = word_tokenize(text, engine='newmm')
     matching = [s for s in proc if ('กิน' in s) or (
         'อาหาร' in s) or ('อะไร' in s)]
     if len(matching) != 0:
         i = randint(0, len(food_list)-1)
         line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=food_list[i]))
+            rtoken, TextSendMessage(text=food_list[i]))
     else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(
+        line_bot_api.reply_message(rtoken, TextSendMessage(
             text='ต้องการสุ่มอาหารหรือเปล่า หากต้องการสุ่ม พิมพ์ กินอะไรดี'))
+    # line_bot_api.reply_message(rtoken, TextSendMessage(text='Hello World!'))
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
